fd77add5e760527e72fd1619f51d18e95ff4b06d
Biggies-desu <bigzaza7993@gmail.com>
2022-09-20 23:14:56 +0700

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './css/styles.css';
-
+import 'bootstrap';
 
 function App() {
   return (
@@ -8,16 +8,16 @@ function App() {
       <header className="App-header">
         <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <meta name="description" content="Numerical Method React project for education." />
         <meta name="author" content="TechnicalMaid :)" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous"></link>
         </head>
         <body>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <div class="container-fluid">
             <a class="navbar-brand" href="#">
-            Numerical Method
+            &emsp;Numerical Method
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
@@ -28,7 +28,7 @@ function App() {
                   <a class="btn btn-dark dropdown-toggle bg-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Root of Equation
                   </a>
-                  <ul class="dropdown-menu dropdown-  dark">
+                  <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="#">Bisection Method</a></li>
                     <li><a class="dropdown-item" href="#">False-Position Method</a></li>
                     <li><a class="dropdown-item" href="#">One-point Iteration Method</a></li>
@@ -52,25 +52,28 @@ function App() {
                     <li><a class="dropdown-item" href="#">Conjugate Gradient Iteration</a></li>
                   </ul>
                 </li>
-                
               </ul>
-              
             </div>
-            
           </div>
+          <a class="navbar-brand" href="#">
+            &emsp;Decha Boonmapasap&emsp;6404062630350&emsp;
+          </a>
         </nav>
+        
         <div class="container">
-            <div class="text-center mt-5">
-                <h1>NUMERICAL METHOD</h1>
-                <p class="lead">คือ กุไม่รู้ไง</p>
-                <p>เดชา บุญมาพาทรัพย์</p>
+            <div class="text-center fw-bold text-dark position-absolute top-50 start-50 translate-middle fs-3">
+                <p>&#128514;&#128514;&#128514;&#128514;&#128514;&#128514;</p>
+                <p>Numerical Method</p>
+                <p>By</p>
+                <p>Biggies-desu</p>
+                <p>&#128151;&#128151;&#128151;&#128151;&#128151;&#128151;</p>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="js/scripts.js"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js "></script>
+        <script src="js/scripts.js"></script>
+        
         
 
     </body>
@@ -78,5 +81,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
